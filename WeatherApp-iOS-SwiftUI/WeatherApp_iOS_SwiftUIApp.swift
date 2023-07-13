432def1160ca71769799621ac61c3031200d818f
muratmnz <minazzmurat@gmail.com>
2023-07-13 09:58:12 +0300

    Initial Commit

---
+++ WeatherApp-iOS-SwiftUI/WeatherApp_iOS_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_iOS_SwiftUIApp.swift
+//  WeatherApp-iOS-SwiftUI
+//
+//  Created by Murat Minaz on 13.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_iOS_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
