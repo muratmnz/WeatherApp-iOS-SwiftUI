b1de73279cda3761b66b6b870e9bfc789fa5f822
muratmnz <minazzmurat@gmail.com>
2023-07-15 22:08:00 +0300

    feat : added LocationManager which includes init, requestLocation , two funcs about Location

---
+++ WeatherApp-iOS-SwiftUI/Services/LocationServer.swift
@@ -0,0 +1,37 @@
+//
+//  LocationManager.swift
+//  WeatherApp-iOS-SwiftUI
+//
+//  Created by Murat Minaz on 15.07.2023.
+//
+
+import Foundation
+
+import CoreLocation
+
+class LocationServer: NSObject, ObservableObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    
+    //location & isloading state
+    @Published var location : CLLocationCoordinate2D?
+    @Published var isLoading = false
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+    
+    func requestLocation(){
+        isLoading = true
+        manager.requestLocation()
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations.first?.coordinate
+        isLoading = false
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error getting location",error)
+        isLoading = false
+    }
+}
